f144688f4cfb5c0908564aa68cb10fb4aa0a5d8c
Nicolas Bertrand <bertrand.nicolas.90@gmail.com>
2018-11-05 16:57:29 +0100

    Improve first statup handling

--- init.el
+++ init.el
@@ -1,16 +1,30 @@
+(setq-local tangle-init-org-file--first-time t)
+
 (defun tangle-init-org-file ()
   "Reload and byte-compile init file from the main org file."
   (interactive)
-  (let ((default-directory user-emacs-directory))
-    (require 'org)
-    ;; tangle `README.org'
-    (org-babel-tangle-file "README.org")
-    ;; load the tangled file to ensure compilation works properly
-    (load-file "README.el")
-    (byte-compile-file "README.el" t)
-    ;; set byte-compiled file as init file
-    (rename-file "README.elc" "init.elc" t)
-    ;; keep a backup of generated init file for debugging purposes
-    (rename-file "README.el" ".init.el.bak" t)))
+  (require 'ob-tangle)
+  ;; For some reason, I can't make `org-babel-load-file' create the
+  ;; .elc file during the first init, so for now, stick to manual
+  ;; tangle -> compile -> load.
+  (let* ((org-file (expand-file-name "README.org" user-emacs-directory))
+         (init-file (expand-file-name "init" user-emacs-directory))
+         (el-file (car (org-babel-tangle-file org-file nil "emacs-lisp"))))
+    (rename-file el-file init-file t)
+    ;; When first loading the config, load the tangled file anyway to
+    ;; ensure that packages are setup.
+    (when (bound-and-true-p tangle-init-org-file--first-time)
+      (load-file init-file))
+    (byte-compile-file init-file t)
+    ;; Keep the init file for debug purposes.
+    (if debug-on-error
+        (rename-file init-file (concat init-file ".el.bak") t)
+      (delete-file init-file))))
+
+(defun tangle-init-org-file-after-save ()
+  (add-hook 'after-save-hook #'tangle-init-org-file nil t))
+
+;; A bit ugly but it does the job
+(setq safe-local-variable-values '((eval tangle-init-org-file-after-save)))
 
 (tangle-init-org-file)
