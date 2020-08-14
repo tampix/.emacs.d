92ab26ec20281b697aff21660c636020c3d4710d
Nicolas Bertrand <bertrand.nicolas.90@gmail.com>
2020-08-15 00:57:14 +0200

    Improve init.el

--- init.el
+++ init.el
@@ -1,30 +1,55 @@
-(setq-local tangle-init-org-file--first-time t)
+(defun in-emacs-dir (file)
+  "Return the absolute path to FILE in `user-emacs-directory'."
+  (file-truename (expand-file-name file user-emacs-directory)))
 
-(defun tangle-init-org-file ()
-  "Reload and byte-compile init file from the main org file."
-  (interactive)
-  (require 'ob-tangle)
+(defvar tangle-init-org-file--first-time
+  (not (file-exists-p (in-emacs-dir "init.elc")))
+  "t if no init.elc exists.")
+
+(defun tangle-init-org-file (&optional no-compile)
+  "Tangle, `byte-compile' and load the init file from README.org.
+
+With a prefix-argument, or a not-nil NO-COMPILE argument if
+called non-interactively, don't compile the tangled file.
+
+Return the absolute path to the tangle file if either NO-COMPILE
+is nil or `debug-on-error' is not-nil, else return nil."
+  (interactive "P")
+  (unless (boundp 'org-babel-tangle-file)
+    (require 'ob-tangle))
   ;; For some reason, I can't make `org-babel-load-file' create the
   ;; .elc file during the first init, so for now, stick to manual
   ;; tangle -> compile -> load.
-  (let* ((org-file (expand-file-name "README.org" user-emacs-directory))
-         (init-file (expand-file-name "init" user-emacs-directory))
-         (el-file (car (org-babel-tangle-file org-file nil "emacs-lisp"))))
-    (rename-file el-file init-file t)
+  (let ((tangled (thread-last
+                   ;; car isn't always default tangled file when there
+                   ;; are multiple tangled files
+                   (org-babel-tangle-file (in-emacs-dir "README.org")
+                                          nil
+                                          "emacs-lisp")
+                   (member (in-emacs-dir "README.el"))
+                   car)))
     ;; When first loading the config, load the tangled file anyway to
     ;; ensure that packages are setup.
-    (when (bound-and-true-p tangle-init-org-file--first-time)
-      (load-file init-file))
-    (byte-compile-file init-file t)
-    ;; Keep the init file for debug purposes.
-    (if debug-on-error
-        (rename-file init-file (concat init-file ".el.bak") t)
-      (delete-file init-file))))
+    (when tangle-init-org-file--first-time
+      (load-file tangled))
+    (if no-compile
+        tangled
+      (byte-compile-file tangled t)
+      (rename-file (byte-compile-dest-file tangled)
+                   "init.elc"
+                   :ok-if-already-exists)
+      (setq tangle-init-org-file--first-time nil)
+      ;; Keep the init file for debug purposes.
+      (if debug-on-error
+          tangled
+        (delete-file tangled)
+        nil))))
 
 (defun tangle-init-org-file-after-save ()
   (add-hook 'after-save-hook #'tangle-init-org-file nil t))
-
-;; A bit ugly but it does the job
+;; It's a bit ugly but it does the job
 (setq safe-local-variable-values '((eval tangle-init-org-file-after-save)))
 
-(tangle-init-org-file)
+(when (and (not after-init-time) ; avoid recursive tangle
+           tangle-init-org-file--first-time)
+  (tangle-init-org-file))
