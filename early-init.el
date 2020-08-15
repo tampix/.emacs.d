253e51bb6bc13650d967ddc36bde11012bb56084
Nicolas Bertrand <bertrand.nicolas.90@gmail.com>
2020-08-15 02:11:47 +0200

    Add early-init.el

---
+++ early-init.el
@@ -0,0 +1,5 @@
+(setq gc-cons-threshold most-positive-fixnum)
+
+(push '(tool-bar-lines . 0) default-frame-alist)
+(push '(menu-bar-lines . 0) default-frame-alist)
+(push '(vertical-scroll-bars) default-frame-alist)
